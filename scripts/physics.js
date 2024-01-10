be12ff5986383e528c563bbd4909f2ca2346355b
confusion <xsadme8499@gmail.com>
2024-01-09 19:04:16 -0500

    added player controls

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,22 +1,20 @@
-window.onload = init;
-
 //Constants
-var INITIAL       = 10,
+const INITIAL       = 10,
     GRAVITY       =  0,
-    REBOUND_RATIO =  0.8,
+    REBOUND_RATIO =  0.5,
     DAMPING_RATIO =  0.995;
 
 //Globals
 var timer, t = 0,
     objects = [],
-    mX = window.innerWidth/2, 
-    mY = window.innerHeight/2;
+    mX = window.innerWidth / 2, 
+    mY = window.innerHeight / 2;
 
 function init(){
   //Start the timer
   addSoccer(mX, mY, 50, 50)
-  addP1(window.innerWidth / 4, mY, 50, 50)
-  addP2((window.innerWidth / 4) * 3, mY, 50, 50)
+  addP1(window.innerWidth / 4, mY, 100, 75)
+  addP2((window.innerWidth / 4) * 3, mY, 100, 75)
   
   timer = window.setInterval(update, 10);
 }
@@ -30,9 +28,9 @@ function addSoccer(x, y, w, h){
   element.style.height = h+"px";
   element.id = "soccerBall"
 
-  document.body.appendChild(element);
+  document.getElementById('gameField').appendChild(element);
   //Create object
-  object = {
+  let object = {
     element: element,
     width:  w,
     height: h,
@@ -52,9 +50,9 @@ function addP1(x, y, w, h){
   element.style.height = h+"px";
   element.id = "p1"
 
-  document.body.appendChild(element);
+  document.getElementById('gameField').appendChild(element);
   //Create object
-  object = {
+  let object = {
     element: element,
     width:  w,
     height: h,
@@ -74,9 +72,9 @@ function addP2(x, y, w, h){
   element.style.height = h+"px";
   element.id = "p2"
 
-  document.body.appendChild(element);
+  document.getElementById('gameField').appendChild(element);
   //Create object
-  object = {
+  let object = {
     element: element,
     width:  w,
     height: h,
@@ -93,49 +91,78 @@ function addForce(obj, x, y){
 }
 
 function update(){
-  for(i=objects.length-1; i>=0; i--){
-    //Apply collisions
-    if(objects[i].px < 0){
-      //Left
+  for(let i = objects.length - 1; i >= 0; i--){
+    // Apply rebound after collision
+    if (objects[i].px < 0) {
+      // Left
       objects[i].px = 0;
-      objects[i].vx *= -REBOUND_RATIO;
-    }
-    else if(objects[i].px + objects[i].width > window.innerWidth-10){
-      //Right
+      objects[i].vx = Math.abs(objects[i].vx) * REBOUND_RATIO;
+    } else if (objects[i].px + objects[i].width > window.innerWidth - 10) {
+      // Right
       objects[i].px = window.innerWidth - objects[i].width - 10;
-      objects[i].vx *= -REBOUND_RATIO;
+      objects[i].vx = -Math.abs(objects[i].vx) * REBOUND_RATIO;
     }
-    if(objects[i].py < 0){
-      //Top
+    if (objects[i].py < 0) {
+      // Top
       objects[i].py = 0;
-      objects[i].vy *= -REBOUND_RATIO;
-    } 
-    else if(objects[i].py + objects[i].height > window.innerHeight-10){
-      //Bottom
+      objects[i].vy = Math.abs(objects[i].vy) * REBOUND_RATIO;
+    } else if (objects[i].py + objects[i].height > window.innerHeight - 10) {
+      // Bottom
       objects[i].py = window.innerHeight - objects[i].height - 10;
-      objects[i].vy *= -REBOUND_RATIO;
+      objects[i].vy = -Math.abs(objects[i].vy) * REBOUND_RATIO;
     }
+
     //Apply damping
     objects[i].vx *= DAMPING_RATIO;
     objects[i].vy *= DAMPING_RATIO;
+
     //Apply gravity
     objects[i].vy += GRAVITY;
+
     //Update position
     objects[i].px += objects[i].vx;
     objects[i].py += objects[i].vy;
     objects[i].element.style.left = objects[i].px+"px";
     objects[i].element.style.top  = objects[i].py+"px";
+    
+    //Update Rotation
+    deg = Math.atan2(objects[i].vy, objects[i].vx) * 180 / Math.PI
+    objects[i].element.style.transform = `rotate(${deg}deg)`
   }
   //Increment time
   t++;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  
-})
+  init()
 
-document.addEventListener('keydown', function(event) {
-  if (event.key == "w") {
-    addForce(objects[1], 0, 10);
-  }
+  document.addEventListener('keydown', function(event) {
+    //Player 1 controls
+    if (event.key == "w") {
+      addForce(objects[1], 0, -2.5);
+    }
+    if (event.key == "a") {
+      addForce(objects[1], -2.5, 0);
+    }
+    if (event.key == "s") {
+      addForce(objects[1], 0, 2.5);
+    }
+    if (event.key == "d") {
+      addForce(objects[1], 2.5, 0);
+    }
+
+    //Player 2 controls
+    if (event.key == "ArrowUp") {
+      addForce(objects[2], 0, -2.5);
+    }
+    if (event.key == "ArrowLeft") {
+      addForce(objects[2], -2.5, 0);
+    }
+    if (event.key == "ArrowDown") {
+      addForce(objects[2], 0, 2.5);
+    }
+    if (event.key == "ArrowRight") {
+      addForce(objects[2], 2.5, 0);
+    }
+  })
 })
